e7e9c5adf2c634f7bcd7aca44a76d0447761b642
Tim Aspesberro <taspes@gmail.com>
2022-11-28 20:25:26 -0500

    added code to javascript file, some html changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//grabs the save button
+var saveBtn = $(".save");
+
+//displays current day in the header element
+var getCurrentDay = () => {
+  var now = new Date();
+
+  var hour = now.getHours();
+  var day = now.getDay();
+  var month = now.getMonth();
+
+	var p = document.createElement('p');
+  p.innerHTML = `Date: ${month}/${day}`;
+
+  document.getElementById('currentDay').appendChild(p);
+}
+
+//Changes color of the time slots to represent past, present and future states
+function slotColor() {
+  var now = new Date();
+  var current = now.getHours();
+  var timeBlock = $(".time-block");
+
+  timeBlock.each(function() {
+      var hourString = parseInt($(this).attr("id"));
+
+      if (hourString > current) {
+          $(this).addClass("future");
+      } else if (hourString === current) {
+          $(this).addClass("present");
+      } else {
+          $(this).addClass("past");
+      }
+  })
+};
+
+//Saves user inputs to local storage
+saveBtn.on("click", function() {
+
+  var timeKey = $(this).siblings(".hour").text();
+  var textValue = $(this).siblings(".inputText").val();
+  localStorage.setItem(timeKey, textValue);
 });
+
+//Repopulates text content
+function useScheduler() {
+
+  $(".hour").each(function() {
+
+      var currText = $(this).text();
+      var currInput = localStorage.getItem(currText);
+
+      if(currInput) {
+          $(this).siblings(".inputText").val(currInput);
+      }
+  });
+}
+
+//involes the functions
+getCurrentDay();
+slotColor();
+useScheduler();
+
